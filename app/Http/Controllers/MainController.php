206aca4f194b1ce3d6e0f80e4559164a58375d2e
Victor <victorhenriquesan@outlook.com>
2024-12-30 21:28:44 -0300

    005

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,20 +20,74 @@ class MainController extends Controller
             'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
             'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
             'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
-
-            'number_one' => 'required|integer|min:0|max:999',
+            'number_one' => 'required|integer|min:0|max:999|lt:number_two',
             'number_two' => 'required|integer|min:0|max:999',
             'number_exercises' => 'required|integer|min:5|max:50',
         ]);
+        //buscar operações selecionadas
+        $operations = [];
+        $operations[] = $request->check_sum ? 'sum' : '';
+        $operations[] = $request->ckeck_subtraction ? 'subtraction' : '';
+        $operations[] = $request->check_multiplication ? 'multiplication' : '';
+        $operations[] = $request->check_division ? 'division' : '';
 
-        dd($request->all());
+        $min = $request->number_one;
+        $max = $request->number_two;
+
+        $numberExercises = $request->number_exercises;
+
+        //gerar os exercicios randomicos
+        // começa como uma coleção vazia
+        $exercices = [];
+        //conjunto de ciclos = numero de exercicios
+        for ($index = 1; $index <= $numberExercises; $index++) {
+
+            // definimos o tipo de operação conforme é preenchido a coleção acima
+            $operation = $operations[array_rand($operations)];
+            // numero 1 é randomico entre o min e max informados no formulario
+            $number1 = rand($min, $max);
+            // numero 2 a mesma coisa
+            $number2 = rand($min, $max);
+
+            $exercise = '';
+            $solution = '';
+
+            switch ($operation) {
+                case 'sum':
+                    $exercise = "$number1 + $number2 = ";
+                    $solution = $number1 + $number2;
+                    break;
+
+                case 'subtraction':
+                    $exercise = "$number1 - $number2 =";
+                    $solution = $number1 - $number2;
+                    break;
+                case 'multiplication':
+                    $exercise = "$number1 x $number2 =";
+                    $solution = $number1 * $number2;
+                    break;
+                case 'division':
+                    $exercise = "$number1 / $number2 =";
+                    $solution = $number1 / $number2;
+                    break;
+            }
+
+            $exercices[] = [
+                'exercise_number' => $index,
+                'exercise' => $exercise,
+                'solution' => "$exercise $solution"
+            ];
+        }
+        dd($exercices);
     }
-    
-    public function printExercises(){
+
+    public function printExercises()
+    {
         echo "Imprimir exercícios no navegador";
     }
 
-    public function exportExercises(){
+    public function exportExercises()
+    {
         echo "Exportar arquivos para um txt";
     }
 }
